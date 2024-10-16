d673fe0a2b60cf07abfab8bba9f90068d2cd9093
emmanuel-shlimon <emmanuel.shlimon@laplateforme.io>
2024-10-16 15:57:20 +0200

    Changement

--- index.php
+++ index.php
@@ -28,28 +28,28 @@
         <h1>Notre sélection</h1>
         <div class="card-container">
             <div class="card">
-            <a href="view_product.php"><img src="./assets/img/mangakaiju.jpg" alt="Photo article 3" class="card-image-book"></a>
+            <a href="view_product.php"><img src="./assets/img/mangakaiju.jpg" alt="Manga Kaiju n°8" class="card-image-book"></a>
             <h2>Kaiju n°8 tome 1</h2>
                 <p class="price">14.99€</p>
                 <a href="#" class="buy-button">Buy</a>
             </div>
 
             <div class="card">
-            <a href="view_product.php"><img src="./assets/img/dvdbluegiant.jpg" alt="Photo article 3" class="card-image-dvd"></a>
+            <a href="view_product.php"><img src="./assets/img/dvdbluegiant.png" alt="DVD de Blue Giant" class="card-image-dvd"></a>
             <h2>DVD Blue Giant</h2>
                 <p class="price">14.99€</p>
                 <a href="#" class="buy-button">Buy</a>
             </div>
 
             <div class="card">
-                <a href="view_product.php"><img src="./assets/img/figurinenaruto.jpg" alt="Photo article 3" class="card-image-figure"></a>
+                <a href="view_product.php"><img src="./assets/img/figurinenaruto.png" alt="Figurine de Naruto" class="card-image-figure"></a>
                 <h2>TITRE ARTICLE 3</h2>
                 <p class="price">14.99€</p>
                 <a href="#" class="buy-button">Buy</a>
             </div>
         </div>
 
-        <a href="#" class="more-articles">Voir plus d'articles</a>
+        <a href="products.php" class="more-articles">Voir plus d'articles</a>
     </main>
 
 </body>
